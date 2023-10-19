6df21bac451e7a184be3840f1adbc1ab3d3119ac
Aman Jaiswal <amanjaiswal98936@gmail.com>
2023-10-20 02:12:44 +0530

    created Undirected, Unweighted basic functions

---
+++ src/Graph.js
@@ -0,0 +1,56 @@
+// Undirected, Unweighted Graph
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    this.adjacencyList[vertex] = [];
+    console.log("addVertex", this.adjacencyList);
+  }
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+    console.log("addEdge", this.adjacencyList);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => {
+      return v !== vertex2;
+    });
+
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => {
+      return v !== vertex1;
+    });
+
+    console.log("removeEdge", this.adjacencyList);
+  }
+
+  removeVertex(vertex) {
+    let vertexArr = this.adjacencyList[vertex];
+    vertexArr.map((v) => {
+      return this.removeEdge(v, vertex);
+    });
+    delete this.adjacencyList[vertex];
+
+    console.log("removeVertex", this.adjacencyList);
+  }
+}
+
+let graphInstance = new Graph();
+graphInstance.addVertex("A");
+graphInstance.addVertex("B");
+graphInstance.addVertex("C");
+graphInstance.addVertex("D");
+graphInstance.addVertex("E");
+
+graphInstance.addEdge("A", "B");
+graphInstance.addEdge("A", "C");
+graphInstance.addEdge("A", "D");
+graphInstance.addEdge("A", "E");
+graphInstance.addEdge("B", "C");
+graphInstance.addEdge("C", "D");
+graphInstance.addEdge("C", "E");
+graphInstance.addEdge("D", "E");
+
+graphInstance.removeEdge("A", "E");
+
+graphInstance.removeVertex("E");
