24519ca3b3845772c28eb6d42084e202e6fa3cc5
Aman Jaiswal <amanjaiswal98936@gmail.com>
2023-11-09 22:14:21 +0530

    Delete ChangePageTitleWhenMovedToAnotherPage.js

--- ChangePageTitleWhenMovedToAnotherPage.js
+++
@@ -1,9 +0,0 @@
-let activeTitle = 'You have 6 items';
-let inactiveTitle = 'Please come back';
-document.title = activeTitle;
-window.addEventListener('blur', e => {
-    document.title = inactiveTitle;
-});
-window.addEventListener('focus', e => {
-    document.title = activeTitle;
-});
