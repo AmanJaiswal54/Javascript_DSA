6cd084854c993ebc68e8e44cf7bb0de46b20b113
Aman Jaiswal <amanjaiswal98936@gmail.com>
2023-11-14 22:53:21 +0530

    Create PrototypeArraySlice.js

---
+++ src/PrototypeArraySlice.js
@@ -0,0 +1,22 @@
+let arr = [2, 5, 12, 30, 25, 40];
+
+Array.prototype.myOwnSlice = function (lowerLimit, upperLimit) {
+  let result = [];
+  let arr = this;
+  if (upperLimit > lowerLimit) {
+    for (let i = lowerLimit; i < upperLimit; i++) {
+      result.push(arr[i]);
+    }
+    return result;
+  } else if (!upperLimit && lowerLimit < arr.length) {
+    for (let i = lowerLimit; i < arr.length; i++) {
+      result.push(arr[i]);
+    }
+    return result;
+  } else {
+    return ["INVALID INPUT"];
+  }
+};
+
+let result = arr.myOwnSlice(6);
+console.log("result 1", result);
